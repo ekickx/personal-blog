811012c003888539cd4feabebfd66efe46e7a8c4
Ghani Rafif <ghani.rafif.2112@gmail.com>
2021-01-22 16:36:15 +0700

    Fix sitemap and some styling - Fix inconsistent font-size - Fix list indention - Make codeblock more readable in dark-mode - Exclude robots.txt from sitemap

--- src/_data/classes.js
+++ src/_data/classes.js
@@ -2,7 +2,7 @@ module.exports = {
   // Classes that will be added to different elements
   html: "font-normal font-sans leading-relaxed antialiased",
   body:
-    "bg-bodybg text-bodytext text-lg md:text-xl overflow-x-hidden flex flex-col min-h-screen",
+    "bg-bodybg text-bodytext text-base md:text-lg overflow-x-hidden flex flex-col min-h-screen",
   navbar:
     "sticky top-0 z-10 bg-bodybg text-bodytext border-b border-cool-gray-100 dark:border-cool-gray-800 px-4 tracking-wider",
   main: "flex flex-col flex-grow",
